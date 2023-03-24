f4fc870df500c74eb2e6e14c54426053795a0a01
coder-abdo <a.h2020.2007@gmail.com>
2023-03-24 12:59:05 +0200

    fix user type

--- src/components/userLogin.component.tsx
+++ src/components/userLogin.component.tsx
@@ -1,9 +1,10 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { SignOutButton } from "@clerk/nextjs";
 import { type User } from "@clerk/nextjs/dist/api";
 import { type FC } from "react";
 
 type Props = {
-  user: User;
+  user: User | any;
 };
 export const LoginUser: FC<Props> = ({ user }) => {
   return (
@@ -11,7 +12,9 @@ export const LoginUser: FC<Props> = ({ user }) => {
       {/* eslint-disable-next-line @next/next/no-img-element */}
       <img
         className="h-14 w-14 rounded-full"
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
         src={user.profileImageUrl}
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
         alt={user?.username ?? ""}
       />
       <SignOutButton />
