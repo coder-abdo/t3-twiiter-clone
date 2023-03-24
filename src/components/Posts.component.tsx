4db5a4e25bc2636c0058fe1f9fc8df0ca5dc4387
coder-abdo <a.h2020.2007@gmail.com>
2023-03-24 13:42:05 +0200

    add `Image` & relative time

--- src/components/Posts.component.tsx
+++ src/components/Posts.component.tsx
@@ -1,16 +1,27 @@
+import Image from "next/image";
 import { type FC } from "react";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
 import { type RouterOutputs } from "~/utils/api";
 
 type Props = RouterOutputs["posts"]["getAll"][number];
 export const PostsView: FC<Props> = ({ auhtor, post }) => {
+  dayjs.extend(relativeTime);
   return (
     <div className="flex items-center gap-2">
-      {/* eslint-disable-next-line @next/next/no-img-element */}
-      <img
+      <Image
         className="h-14 w-14 rounded-full"
         src={auhtor.profileImageUrl}
         alt={auhtor?.username ?? ""}
+        width={14}
+        height={14}
       />
+      <div className="flex flex-col">
+        <h2 className="text-slate-300">
+          <span className="pr-2">@{auhtor.username}</span>
+          <span>. {dayjs(post.createdAt).fromNow()}</span>
+        </h2>
+      </div>
       <p className="text-lg capitalize text-slate-100">{post.content}</p>
     </div>
   );
--- src/components/userLogin.component.tsx
+++ src/components/userLogin.component.tsx
@@ -1,21 +1,24 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { SignOutButton } from "@clerk/nextjs";
 import { type User } from "@clerk/nextjs/dist/api";
+import Image from "next/image";
 import { type FC } from "react";
 
 type Props = {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   user: User | any;
 };
 export const LoginUser: FC<Props> = ({ user }) => {
   return (
     <div className="flex items-center justify-end gap-2 py-4">
-      {/* eslint-disable-next-line @next/next/no-img-element */}
-      <img
+      <Image
         className="h-14 w-14 rounded-full"
         // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
         src={user.profileImageUrl}
         // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
         alt={user?.username ?? ""}
+        width={14}
+        height={14}
       />
       <SignOutButton />
     </div>
